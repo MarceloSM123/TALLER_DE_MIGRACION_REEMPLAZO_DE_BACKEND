a0213c11a8cc15d4bd8552b2bb0ca3558f0a6001
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-07-16 11:57:29 -0500

    Paso 2: Reescribir los Endpoints de Java a Node.js (CRUD) y Paso 3: Conectar el Frontend en React al Nuevo Backend

--- FrontEnd/jwt_5/src/config/apiConfig.js
+++ FrontEnd/jwt_5/src/config/apiConfig.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = 'http://localhost:8080';
+// export const API_BASE_URL = 'http://localhost:8080';
+// cambiamos al puerto en el que estamos tabajando
+export const API_BASE_URL = 'http://localhost:3000';
--- FrontEnd/jwt_5/src/context/appContext.jsx
+++ FrontEnd/jwt_5/src/context/appContext.jsx
@@ -1,3 +1,5 @@
+import React, { createContext, useContext } from 'react';
+
 const AuthContext1 = createContext();
 
 export function AuthProvider1(props) {
